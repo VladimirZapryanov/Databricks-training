477b1df5a38e602ca5a20be7909a5716736f319e
VladimirZapryanov <VladimirZapryanov>
2023-08-01 09:29:17 +0000

    adding new notebooks

---
+++ my_folder/Basic commands (1).py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Title 1
+# MAGIC ## Title 2
+# MAGIC ### Title 3
+
+# COMMAND ----------
+
+print('Hello Word!')
+
+# COMMAND ----------
+
+# MAGIC %run "/Run Demo/run_demo"
+
+# COMMAND ----------
+
+print(name)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC Order list:
+# MAGIC 1. Beef
+# MAGIC 2. Chicken
+# MAGIC 3. Pork
+# MAGIC
+# MAGIC Unorder list:
+# MAGIC - one 
+# MAGIC - two
+# MAGIC - three
+# MAGIC * one 
+# MAGIC * two
+# MAGIC * three

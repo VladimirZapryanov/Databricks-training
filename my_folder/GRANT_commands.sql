bc789e9cf31fc075474a11753bfcd6fdfaaae292
VladimirZapryanov <VladimirZapryanov>
2023-08-11 14:53:29 +0000

    add new notebook

---
+++ my_folder/GRANT_commands.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+GRANT SELECT, MODIFY, READ_METADATA, CREATE ON SCHEMA hr_db TO hr_team;
+
+GRANT USAGE ON SCHEMA hr_db TO hr_team;
+
+GRANT SELECT ON VIEW hr_db.paris_employees_vw TO adam@mycompany.com;
+
+SHOW GRANTS ON SCHEMA hr_db;
+
+SHOW GRANTS ON VIEW hr_db.paris_employees_vw;

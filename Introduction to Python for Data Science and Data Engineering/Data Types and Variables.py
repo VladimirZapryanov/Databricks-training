b3f2cda4ef94d09e5ce3a5a6ecd0086f2c412695
VladimirZapryanov <VladimirZapryanov>
2023-10-12 09:25:56 +0000

    add more code and lab

--- Introduction to Python for Data Science and Data Engineering/Data Types and Variables.py
+++ Introduction to Python for Data Science and Data Engineering/Data Types and Variables.py
@@ -1,4 +1,10 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Lecture
+
+# COMMAND ----------
+
 # This is our first line of Python code!
 1+1
 
@@ -41,6 +47,87 @@ True and False
 # COMMAND ----------
 
 not False
+
+# COMMAND ----------
+
+a = 3
+b = 2 
+c = a*b
+
+c
+
+# COMMAND ----------
+
+b = 4
+c
+
+# COMMAND ----------
+
+b = "Hello World"
+b = 1
+b
+
+# COMMAND ----------
+
+a = 1
+b = 2
+
+a # This line isn't printed becouse it's not the last line of code  
+b 
+
+# COMMAND ----------
+
+print(a)
+print(b)
+
+# COMMAND ----------
+
+print('Hello World!')
+
+# COMMAND ----------
+
+name = 'Robin'
+age = 30
+
+print(f'My name is {name} and I am {age} years old')
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Lab
+
+# COMMAND ----------
+
+name = 'Vlad'
+num_chocolate = 2
+chocolate_string = f'{name} would like to eat {num_chocolate} bars of chocolate'
+
+print(chocolate_string)
+
+# COMMAND ----------
+
+assert type(name) == str, "Name should be a string"
+assert type(num_chocolate) == int, "You have to eat the entire chocolate bar! No floats allowed"
+assert chocolate_string == f"{name} would like to eat {num_chocolate} bars of chocolate", "Did you mistype something?"
+print("Test passed!")
+
+# COMMAND ----------
+
+num_students = 32
+num_days = 20
+class_name = 'Class Test'
+class_information = f'There are {num_students * num_days} student days in the {class_name}'
+
+print(class_information)
+
+# COMMAND ----------
+
+assert type(class_name) == str, "Name should be a string"
+assert type(num_students) == int, "Use an integer"
+assert type(num_days) == int, "Use an integer"
+assert class_information == f"There are {num_students * num_days} student days in the {class_name}"
+
+print("Test passed!")
 
 # COMMAND ----------
 

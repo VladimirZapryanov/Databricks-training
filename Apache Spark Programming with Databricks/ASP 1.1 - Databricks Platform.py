f7c1ba2c7b9b44492637c5e8252ed4c908944557
VladimirZapryanov <VladimirZapryanov>
2023-11-20 14:30:32 +0000

    add new notebook

---
+++ Apache Spark Programming with Databricks/ASP 1.1 - Databricks Platform.py
@@ -0,0 +1,112 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC # Lecture
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %run "/Apache Spark/apache-spark-programming-with-databricks-english/Includes/Classroom-Setup" 
+
+# COMMAND ----------
+
+print("Run default language")
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC
+# MAGIC print("Run python")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC
+# MAGIC println("Run scala")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select "Run SQL"
+
+# COMMAND ----------
+
+# MAGIC %r
+# MAGIC
+# MAGIC print("Run R")
+
+# COMMAND ----------
+
+# MAGIC %sh ps | grep 'java'
+# MAGIC
+
+# COMMAND ----------
+
+html = """<h1 style="color:orange;text-align:center;font-family:Courier">Render HTML</h1>"""
+
+displayHTML(html)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Create documentation cells
+# MAGIC Render cell as <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">Markdown</a> using the magic command: **`%md`**
+# MAGIC
+# MAGIC Below are some examples of how you can use Markdown to format documentation. Click this cell and press **`Enter`** to view the underlying Markdown syntax.
+# MAGIC
+# MAGIC
+# MAGIC # Heading 1
+# MAGIC ### Heading 3
+# MAGIC > block quote
+# MAGIC
+# MAGIC 1. **bold**
+# MAGIC 2. *italicized*
+# MAGIC 3. ~~strikethrough~~
+# MAGIC
+# MAGIC ---
+# MAGIC
+# MAGIC - <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">link</a>
+# MAGIC - `code`
+# MAGIC
+# MAGIC ```
+# MAGIC {
+# MAGIC   "message": "This is a code block",
+# MAGIC   "method": "https://www.markdownguide.org/extended-syntax/#fenced-code-blocks",
+# MAGIC   "alternative": "https://www.markdownguide.org/basic-syntax/#code-blocks"
+# MAGIC }
+# MAGIC ```
+# MAGIC
+# MAGIC ![Spark Logo](https://files.training.databricks.com/images/Apache-Spark-Logo_TM_200px.png)
+# MAGIC
+# MAGIC | Element         | Markdown Syntax |
+# MAGIC |-----------------|-----------------|
+# MAGIC | Heading         | `#H1` `##H2` `###H3` `#### H4` `##### H5` `###### H6` |
+# MAGIC | Block quote     | `> blockquote` |
+# MAGIC | Bold            | `**bold**` |
+# MAGIC | Italic          | `*italicized*` |
+# MAGIC | Strikethrough   | `~~strikethrough~~` |
+# MAGIC | Horizontal Rule | `---` |
+# MAGIC | Code            | ``` `code` ``` |
+# MAGIC | Link            | `[text](https://www.example.com)` |
+# MAGIC | Image           | `![alt text](image.jpg)`|
+# MAGIC | Ordered List    | `1. First items` <br> `2. Second Item` <br> `3. Third Item` |
+# MAGIC | Unordered List  | `- First items` <br> `- Second Item` <br> `- Third Item` |
+# MAGIC | Code Block      | ```` ``` ```` <br> `code block` <br> ```` ``` ````|
+# MAGIC | Table           |<code> &#124; col &#124; col &#124; col &#124; </code> <br> <code> &#124;---&#124;---&#124;---&#124; </code> <br> <code> &#124; val &#124; val &#124; val &#124; </code> <br> <code> &#124; val &#124; val &#124; val &#124; </code> <br>|
+
+# COMMAND ----------
+
+# MAGIC %fs ls
+
+# COMMAND ----------
+
+# MAGIC %fs ls /databricks-datasets
+
+# COMMAND ----------
+
+# MAGIC %fs head /databricks-datasets/README.md
+
+# COMMAND ----------
+
+

41f13c65eaef7001a13137b40ea246e58abfba17
VladimirZapryanov <VladimirZapryanov>
2023-11-27 15:23:20 +0000

    add new norebook

--- Apache Spark Programming with Databricks/ASP 2.3 - DataFrame & Column.py
+++ Apache Spark Programming with Databricks/ASP 2.3 - DataFrame & Column.py
@@ -27,4 +27,209 @@ print(col('device'))
 
 # COMMAND ----------
 
+col("ecommerce.purchase_revenue_in_usd") + col("ecommerce.total_item_quantity")
+col("event_timestamp").desc()
+(col("ecommerce.purchase_revenue_in_usd") * 100).cast("int")
+
+# COMMAND ----------
+
+rev_df = (events_df
+         .filter(col("ecommerce.purchase_revenue_in_usd").isNotNull())
+         .withColumn("purchase_revenue", (col("ecommerce.purchase_revenue_in_usd") * 100).cast("int"))
+         .withColumn("avg_purchase_revenue", col("ecommerce.purchase_revenue_in_usd") / col("ecommerce.total_item_quantity"))
+         .sort(col("avg_purchase_revenue").desc())
+        )
+
+display(rev_df)
+
+# COMMAND ----------
+
+devices_df = events_df.select("user_id", "device")
+display(devices_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+locations_df = events_df.select(
+    "user_id", 
+    col("geo.city").alias("city"), 
+    col("geo.state").alias("state")
+)
+display(locations_df)
+
+# COMMAND ----------
+
+apple_df = events_df.selectExpr("user_id", "device in ('macOS', 'iOS') as apple_user")
+display(apple_df)
+
+# COMMAND ----------
+
+anonymous_df = events_df.drop("user_id", "geo", "device")
+display(anonymous_df)
+
+# COMMAND ----------
+
+no_sales_df = events_df.drop(col("ecommerce"))
+display(no_sales_df)
+
+
+# COMMAND ----------
+
+mobile_df = events_df.withColumn("mobile", col("device").isin("iOS", "Android"))
+display(mobile_df)
+
+# COMMAND ----------
+
+purchase_quantity_df = events_df.withColumn("purchase_quantity", col("ecommerce.total_item_quantity").cast("int"))
+purchase_quantity_df.printSchema()
+
+# COMMAND ----------
+
+location_df = events_df.withColumnRenamed("geo", "location")
+display(location_df)
+
+# COMMAND ----------
+
+purchases_df = events_df.filter("ecommerce.total_item_quantity > 0")
+display(purchases_df)
+
+# COMMAND ----------
+
+revenue_df = events_df.filter(col("ecommerce.purchase_revenue_in_usd").isNotNull())
+display(revenue_df)
+
+
+# COMMAND ----------
+
+android_df = events_df.filter((col("traffic_source") != "direct") & (col("device") == "Android"))
+display(android_df)
+
+# COMMAND ----------
+
+display(events_df.distinct())
+
+# COMMAND ----------
+
+distinct_users_df = events_df.dropDuplicates(["user_id"])
+display(distinct_users_df)
+
+# COMMAND ----------
+
+limit_df = events_df.limit(100)
+display(limit_df)
+
+# COMMAND ----------
+
+increase_timestamps_df = events_df.sort("event_timestamp")
+display(increase_timestamps_df)
+
+# COMMAND ----------
+
+decrease_timestamp_df = events_df.sort(col("event_timestamp").desc())
+display(decrease_timestamp_df)
+
+# COMMAND ----------
+
+increase_sessions_df = events_df.orderBy(["user_first_touch_timestamp", "event_timestamp"])
+display(increase_sessions_df)
+
+# COMMAND ----------
+
+decrease_sessions_df = events_df.sort(col("user_first_touch_timestamp").desc(), col("event_timestamp"))
+display(decrease_sessions_df)
+
+# COMMAND ----------
+
+DA.cleanup()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Lab
+
+# COMMAND ----------
+
+events_df = spark.read.format("delta").load(DA.paths.events)
+display(events_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+revenue_df = events_df.select(
+    "*", 
+    col("ecommerce.purchase_revenue_in_usd").alias("revenue")
+)
+display(revenue_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+expected1 = [5830.0, 5485.0, 5289.0, 5219.1, 5180.0, 5175.0, 5125.0, 5030.0, 4985.0, 4985.0]
+result1 = [row.revenue for row in revenue_df.sort(col("revenue").desc_nulls_last()).limit(10).collect()]
+
+assert(expected1 == result1)
+print("All test pass")
+
+# COMMAND ----------
+
+purchases_df = revenue_df.filter(col("revenue").isNotNull())                   
+display(purchases_df)
+
+# COMMAND ----------
+
+assert purchases_df.filter(col("revenue").isNull()).count() == 0, "Nulls in 'revenue' column"
+print("All test pass")
+
+# COMMAND ----------
+
+distinct_df = purchases_df.select("event_name").distinct()
+display(distinct_df)
+
+distinct_df2 = purchases_df.dropDuplicates(["event_name"])
+display(distinct_df2)
+
+# COMMAND ----------
+
+final_df = purchases_df.drop("event_name")
+display(final_df)
+
+# COMMAND ----------
+
+expected_columns = {"device", "ecommerce", "event_previous_timestamp", "event_timestamp",
+                    "geo", "items", "revenue", "traffic_source",
+                    "user_first_touch_timestamp", "user_id"}
+assert(set(final_df.columns) == expected_columns)
+print("All test pass")
+
+# COMMAND ----------
+
+final_df = (events_df
+            .withColumn("revenue", col("ecommerce.purchase_revenue_in_usd"))
+            .filter(col("revenue").isNotNull())
+            .drop("event_name")
+           )
+
+display(final_df)
+
+# COMMAND ----------
+
+assert(final_df.count() == 180678)
+print("All test pass")
+
+# COMMAND ----------
+
+expected_columns = {"device", "ecommerce", "event_previous_timestamp", "event_timestamp",
+                    "geo", "items", "revenue", "traffic_source",
+                    "user_first_touch_timestamp", "user_id"}
+assert(set(final_df.columns) == expected_columns)
+print("All test pass")
+
+# COMMAND ----------
+
+DA.cleanup()
+
+# COMMAND ----------
+
 

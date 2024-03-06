ecb1b9930a16e254c008c16501e653e359ffbe22
VladimirZapryanov <VladimirZapryanov>
2024-03-06 15:02:20 +0000

    add new notebook

--- Databricks Certified Associate Developer for Apache Spark using Pyspark/75 - Perform Grouped Aggregations using direct functions on a Spark Data Frame.py
+++ Databricks Certified Associate Developer for Apache Spark using Pyspark/75 - Perform Grouped Aggregations using direct functions on a Spark Data Frame.py
@@ -1,2 +1,15 @@
 # Databricks notebook source
 order_items = spark.read.json('/FileStore/public/retail_db_json/order_items')
+
+# COMMAND ----------
+
+order_items_grouped = order_items. \
+    groupBy('order_item_order_id')
+
+# COMMAND ----------
+
+type(order_items_grouped)
+
+# COMMAND ----------
+
+

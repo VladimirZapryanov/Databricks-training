50a3b91c9d50cd9e4db081f801a356efbe42f0b4
VladimirZapryanov <VladimirZapryanov>
2024-03-21 11:07:14 +0000

    add new notebooks

---
+++ Databricks Certified Associate Developer for Apache Spark using Pyspark/118 - Using Spark UDFs as part of Data Frame APIs.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+help(spark.udf.register)
+
+# COMMAND ----------
+
+df = spark.read.json('/FileStore/public/retail_db_json/orders')
+
+# COMMAND ----------
+
+df.show()
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+dc = spark.udf.register('date_convert', lambda d: int(d[:10].replace('-', '')))
+
+# COMMAND ----------
+
+dc
+
+# COMMAND ----------
+
+df.select(dc('order_date').alias('order_date')).show()
+
+# COMMAND ----------
+
+df.filter(dc('order_date') == 20140101).show()
+
+# COMMAND ----------
+
+df. \
+    groupBy(dc('order_date').alias('order_date')). \
+    count(). \
+    withColumnRenamed('count', 'order_count'). \
+    show()
+
+# COMMAND ----------
+
+
